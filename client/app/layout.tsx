bcbe9fe848a4d203f770ac5e17e4d448a093a8b6
abdelrhman osama <abdalrhman.osama.mostafa@gmail.com>
2025-09-16 21:14:32 +0300

    1

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -8,6 +8,9 @@ import Nav from "@/components/nav";
 export const metadata: Metadata = {
   title: "muscular Store",
   description: "store for gym supplements",
+    other: {
+    "6a97888e-site-verification": "673dff43759a9f5951315b4f66201ccf"
+  }
 };
 
 export default function RootLayout({
