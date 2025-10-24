6d37e425209884cb2d7a0a43ce9c08bbaf1672fe
abdelrhman osama <abdalrhman.osama.mostafa@gmail.com>
2025-10-25 01:07:32 +0300

    1

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,12 +1,9 @@
 
 require('dotenv').config();
-const Redis = require("ioredis");
-    const redis = new Redis({
-  host: process.env.REDIS_HOST ,
-  port: process.env.REDIS_PORT ,
-  username: process.env.REDIS_USERNAME ,  
-  password: process.env.REDIS_PASSWORD ,
+const { createClient } = require("redis");
 
+const redis = createClient({
+  url: process.env.REDIS_URL,
 });
 
 redis.on("connect", () => {
@@ -17,4 +14,9 @@ redis.on("error", (err) => {
   console.error("❌ Redis error:", err);
 });
 
-module.exports = redis; 
+// 🔑 مهم: الاتصال
+(async () => {
+  await redis.connect();
+})();
+
+module.exports = redis;
